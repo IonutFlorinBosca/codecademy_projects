b2293cc8a2001fa8d8af42fc805406009c88368c
Ionut <ionutflorinbosca@yahoo.com>
2022-04-16 10:00:12 +0200

    Functionality added to the program! Whoohoo - first time I am integrating JS with HTML

--- Message_generator_program/message_generator.js
+++ Message_generator_program/message_generator.js
@@ -37,4 +37,5 @@ let quotes = [
 
 function newQuote() {
     let randomNumber = Math.floor(Math.random() * (quotes.length));
+    document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
 }
